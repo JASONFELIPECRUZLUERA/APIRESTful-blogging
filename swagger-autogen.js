6face496252dc671a1a8c3ce1638455bb3c9cba0
JASON <jasonfelipecruzluera@gmail.com>
2024-05-03 08:15:08 -0500

    title swagger

--- swagger-autogen.js
+++ swagger-autogen.js
@@ -2,16 +2,13 @@ import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    title: 'API RESTful BLOGGING',
+    description: 'responsible for creating, reading, modifying and deleting user data, posts, comments and categories'
   },
   host: 'localhost:3000'
 };
 
 const outputFile = './swagger-output.json';
 const routes = ['./index.js'];
-
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 swaggerAutogen()(outputFile, routes, doc);
